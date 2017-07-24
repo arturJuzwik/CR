1360297d72a7797fec5b736606346968db65a508
arturJuzwik <artjuzwik@gmail.com>
2017-07-24 16:08:55 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //SMOOTH SCROLL
-$('body').scrollspy({target:'.navbar',offset:50});
 $('#navbar-main a').on('click',function(event){
 	if (this.hash !== ""){
 		event.preventDefault();
@@ -12,47 +11,68 @@ $('#navbar-main a').on('click',function(event){
 	}
 });
 
-//navbar padding
-var navbar = document.querySelector('.navigation');
-
+//navbar padding and color
+const navbar = document.querySelector('.navigation'),
+		a = document.querySelectorAll('.navigation-ul li a'),
+		svgPaths = document.querySelectorAll('.filler'),
+		navbarCollapse = document.querySelector('.navbar-collapse');
+    		
 window.addEventListener('scroll', function (){
-	if (window.pageYOffset > 80)
-		navbar.classList.add('navigation-js')
-	else
-		navbar.classList.remove('navigation-js')
+
+		
+if (window.pageYOffset > 80){
+	navbar.classList.add('navigation-js');
+		for (let i=0; i<svgPaths.length; i++){
+			svgPaths[i].classList.add('filler-black');
+		}
+		for (let i=1; i<a.length;i++){
+			a[i].classList.add('li-js');
+		}
+
+		
+}else{
+	navbar.classList.remove('navigation-js')
+		for (let i=0; i<svgPaths.length; i++){
+			svgPaths[i].classList.remove('filler-black');
+		}
+		for(var i=0; i<a.length;i++){
+			a[i].classList.remove('li-js');
+			
+ 		}
+}
+
+			
 });
 
-//inputs
-const labelText = document.querySelector('.input-label-one'),
-		inputText = document.querySelector('.login-form-text'),
-		removerFirst = document.querySelector('.login-form-icon-remove-first'),
-		inputPassword = document.querySelector('.login-form-password'),
-		removerSecond = document.querySelector('.login-form-icon-remove-second');
+//show password
+const showButton = document.querySelector('.login-form-icon-show'),
+		inputPassword = document.querySelector('.login-form-password');
+let	shown = 0;
 
-//first input
-labelText.addEventListener('keypress',function(){
-	if(inputText.value.length > 1){
-		removerFirst.classList.add('show');
-	}else
-		removerFirst.classList.remove('show');
+$('.login-form-icon-remove').click(
+    function(){
+        $('.login-form-text').val('');
+    });
+
+
+function show(){
+	inputPassword.setAttribute('type','text');
+}
+function hide(){
+	inputPassword.setAttribute('type','password');
+}
+showButton.addEventListener('click', function(){
+	if (shown == 0){
+		showButton.classList.remove('glyphicon-eye-open');
+		showButton.classList.add('glyphicon-eye-close');
+		shown = 1;
+		show();
+	}else{
+		showButton.classList.remove('glyphicon-eye-close');
+		showButton.classList.add('glyphicon-eye-open');
+		shown=0;
+		hide();
+	}
 });
-//	
-////		removerFirst.classList.add('show');
-//		window.onclick = function(event){
-//			if (!event.target.matches('.login-form-text')){
-//				removerFirst.classList.remove('show');
-//			}
-//		}
-//});
-//	//second input
-//
-//inputPassword.addEventListener('click',function(){
-//	
-//
-//		removerSecond.classList.add('show');
-//		window.onclick = function(event){
-//			if (!event.target.matches('.login-form-password')){
-//				removerSecond.classList.remove('show');
-//			}
-//		}
-//});
+
+
